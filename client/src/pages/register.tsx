4d02a3c4f41347c0aeb3106ccf3fb1c7b14be883
nullmage <jsky.johnson@gmail.com>
2020-09-23 13:03:35 -0600

    Fixes cookies issue

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -12,6 +12,7 @@ import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useMutation } from "urql";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
 
 interface registerProps {}
 
@@ -21,9 +22,12 @@ const Register: React.FC<registerProps> = ({}) => {
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           //console.log(values);
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          }
         }}
       >
         {({ isSubmitting }) => (
