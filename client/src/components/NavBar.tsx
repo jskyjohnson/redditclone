c2398abc8bdd93a12d210d078743caa245fc5108
nullmage <jsky.johnson@gmail.com>
2020-09-23 22:06:18 -0600

    Adds logout

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { Box, Button, Flex, Link } from "@chakra-ui/core";
 import React from "react";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
 
   let body = null;
@@ -27,7 +28,15 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={4}>{data.me.username}</Box>
-        <Button variant="link"> logout </Button>
+        <Button
+          variant="link"
+          isLoading={logoutFetching}
+          onClick={() => {
+            logout();
+          }}
+        >
+          logout
+        </Button>
       </Flex>
     );
 
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
 import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
 import {
   LoginMutation,
+  LogoutMutation,
   MeDocument,
   MeQuery,
   RegisterMutation,
@@ -33,6 +34,14 @@ const client = createClient({
     cacheExchange({ //updates the cache! 
       updates: {
         Mutation: {
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache, 
+              {query: MeDocument},
+              _result,
+              () => ({me : null})
+            );
+          },
           login: (_result, args, cache, info) => {
             betterUpdateQuery<LoginMutation, MeQuery>(
               cache,
