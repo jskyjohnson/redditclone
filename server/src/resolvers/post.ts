a2ca5819b638ab95afd6ffde3b04cca4db1ba274
nullmage <jsky.johnson@gmail.com>
2020-09-28 23:13:06 -0600

    Adds refreshing

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -126,14 +126,11 @@ export class PostResolver {
   async posts(
     @Arg("limit", () => Int) limit: number,
     @Arg("cursor", () => String, { nullable: true }) cursor: string | null,
-    @Ctx() { req }: MyContext
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
     const replacements: any[] = [realLimitPlusOne];
-
-    
 
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
